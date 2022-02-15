e15ffdf5e0f60d0416563dae7dc87bc48265c3bb
Mark003 <coderush@gmail.com>
2022-02-15 11:31:54 +0100

    Clean up

--- GettingStarted/FirstXpo_CrudOperations.cs
+++ GettingStarted/FirstXpo_CrudOperations.cs
@@ -6,7 +6,6 @@
 		public static void TestAll()
 		{
 			//`![](NextStep;crcommand:TemplateExpand:#CrudCreate#;;0.01200,0.01200)  << Click for sample code to create & save an instance of StatInfo....
-
 		}
 	}
 }
