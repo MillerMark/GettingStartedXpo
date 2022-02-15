936e21d125dc00a97850cf7830183e241e5cc3aa
Mark003 <coderush@gmail.com>
2022-02-15 10:06:46 +0100

    Added lesson summaries. Fixed run issue.

--- GettingStarted/FirstXpo_DatabaseProvider.cs
+++ GettingStarted/FirstXpo_DatabaseProvider.cs
@@ -18,8 +18,8 @@
 
 		Perfect!
 
-		You can close the NuGet Package Manager now 
-		(and the StartHere.cs file) if you like.
+		You can close the NuGet Package Manager now (and 
+		files in the other tab group) if you like.
 		
 		![](NextStep;crcommand:OpenFile:Program.cs;;0.01200,0.01200)  << setting up the data layer....
 																			 */
