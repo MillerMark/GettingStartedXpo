b9cb8906def1120bcded2caf77a7d52aebb99f6e
Mark003 <coderush@gmail.com>
2022-02-18 11:19:09 +0100

    CodeRush template training.

--- GettingStarted/CodeRushTemplatesIntro.cs
+++ GettingStarted/CodeRushTemplatesIntro.cs
@@ -3,8 +3,67 @@
 	//`++CodeRush Templates
 
 	/*
-		This is a placeholder for a quick start on CodeRush Templates.
-		
-		![](GotIt;vscommand:File.Close;;0.01200,0.01200)
+		CodeRush Templates turn easy-to-use shortcut sequences into common 
+		boilerplate code.
+
+		This file contains examples to get you started.
+																				 */
+
+	//`++Setup
+	/*
+		Code Templates expand when you press either Space or Tab. You can 
+		decide which key to use in the Setup Wizard (on the "Code Template 
+		Expansion" page).
+
+		![](CodeTemplateOptions;crcommand:RunSetupWizard;;0.01076,0.01076)  << Run the Setup Wizard
+
+
 																			 */
+
+	public class Car
+	{
+		// To create a new property of type string, enter "ps" on an empty
+		// line followed by your template expansion key (Tab or Space).
+		//`![](ArrowDown;;;0.00862,0.00862)  (name the property "Model" and press Enter).
+
+
+
+		// To create a new method use "m" (and your template expansion key).
+		//`![](ArrowDown;;;0.00862,0.00862)  (name the method "Start")
+
+
+
+		// To create a method that returns a string, use "ms".
+		//`![](ArrowDown;;;0.00862,0.00862)  (name the method "GetBrand")
+
+
+
+		// To create a property of type int, use "pi".
+		//`![](ArrowDown;;;0.00862,0.00862)  (name the method "WheelCount")
+
+
+
+		//`![](GotIt;vscommand:File.Close;;0.01200,0.01200)
+
+		// For more examples, check out these videos:
+
+		//![](T For Tests;https://www.youtube.com/watch?v=Hj8dxfzUPOE&list=PL8h4jt35t1wgawacCN9wmxq1EN36CNUGk&index=11 ;;0.04,0.04) << T for Tests
+
+		//![](E for Enum;https://www.youtube.com/watch?v=Kw83DbF6r5I&list=PL8h4jt35t1wgawacCN9wmxq1EN36CNUGk&index=14 ;;0.04,0.04) << E for Enum
+
+		//![](M For Method;https://www.youtube.com/watch?v=SKjE0vn0j4s&list=PL8h4jt35t1wgawacCN9wmxq1EN36CNUGk&index=15 ;;0.04,0.04) << M for Methods
+
+		//![](P For Properties;https://www.youtube.com/watch?v=olpK5oDWYwY&list=PL8h4jt35t1wgawacCN9wmxq1EN36CNUGk&index=17 ;;0.04,0.04) << P for Properties
+
+		//![](Q for Constant;https://www.youtube.com/watch?v=2o6E8FNP3jY&list=PL8h4jt35t1wgawacCN9wmxq1EN36CNUGk&index=21 ;;0.04,0.04) << Q for Constants
+
+		//![](T for Types;https://www.youtube.com/watch?v=WiGSSxFLNhI&list=PL8h4jt35t1wgawacCN9wmxq1EN36CNUGk&index=22 ;;0.04,0.04) << T for Types
+
+		//![](V for Variable;https://www.youtube.com/watch?v=cG6UPS0Ft5g&list=PL8h4jt35t1wgawacCN9wmxq1EN36CNUGk&index=30 ;;0.05965,0.05965) << V for Variable
+
+		//![](N for New Instance;https://www.youtube.com/watch?v=qqOJLe1hnzw&list=PL8h4jt35t1wgawacCN9wmxq1EN36CNUGk&index=31 ;;0.05965,0.05965) << N for New Instance
+
+		//![](C for Casting;https://www.youtube.com/watch?v=cY1b4CJXA-I&list=PL8h4jt35t1wgawacCN9wmxq1EN36CNUGk&index=32 ;;0.05965,0.05965) << C for Casting
+
+	}
 }
